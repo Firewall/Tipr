afc998723a639f8c1b892a84e88005e6394cbd16
Anthony Meirlaen <anthony.meirlaen@gmail.com>
2014-03-29 15:02:36 +0100

    authentication added

--- src/Tipr/ApplicationBundle/Controller/AuthenticationController.php
+++ src/Tipr/ApplicationBundle/Controller/AuthenticationController.php
@@ -9,11 +9,13 @@
 namespace Tipr\ApplicationBundle\Controller;
 
 
+use GuzzleHttp\Client;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Tipr\ApplicationBundle\Form\Type\LogInType;
+use Httpful\Request as Api;
 
-class AuthenticationController extends Controller
+class AuthenticationController extends BaseController
 {
     public function logInAction()
     {
@@ -26,16 +28,47 @@ class AuthenticationController extends Controller
 
     public function logInProcessAction(Request $request)
     {
+        $error = false;
+
         $form = $this->createForm(new LogInType());
 
         $form->handleRequest($request);
 
         if($form->isValid()){
             $data = $form->getData();
+
+            $url = $this->api_base_url . "/openlogin/rest/ticket" . $this->api_key;
+
+            $json['loginDocument'] = ['documentType' => 0, 'document' => $data['documentNumber']];
+            $json['birthday'] =  $data['birthDate'];
+
+            $response = Api::post($url,json_encode($json), 'application/json')->send();
+
+            $ticket = $response->body->ticket;
+
+            $client = new Client();
+
+            $response = $client->post($this->api_base_url . '/openapi/login/auth/response' . $this->api_key, [
+                'headers' => ['Content-Type' => 'application/x-www-form-urlencoded'],
+                'body'    => ['ticket' => $ticket]
+            ]);
+
+            // get cookie
+            $cookie = $response->getHeader('set-cookie');
+
+            $client = $this->api_get('/openapi/rest/client',$cookie);
+
+            if($cookie && $client->personId){
+                $request->getSession()->set('cookie',$cookie);
+                $request->getSession()->set('personId',$client->personId);
+            }else{
+                $error = 'Incorrect document number or birthday';
+            }
         }
 
         return $this->render('TiprApplicationBundle:Authentication:login.html.twig',array(
-            'form' => $form->createView()
+            'form' => $form->createView(),
+            'error' => $error
         ));
     }
 } 
--- src/Tipr/ApplicationBundle/Controller/BaseController.php
+++ src/Tipr/ApplicationBundle/Controller/BaseController.php
@@ -8,12 +8,12 @@ use GuzzleHttp\Client;
 
 class BaseController extends Controller
 {
-    private $api_base_url = 'https://apisandbox.ingdirect.es';
-    private $api_key = 'IEjdcD3VfCF63pRDE4DrviMi9zA30GI8';
+    public  $api_base_url = 'https://apisandbox.ingdirect.es';
+    public  $api_key = '?apikey=IEjdcD3VfCF63pRDE4DrviMi9zA30GI8';
 
     public function api_get($resource, $cookie)
     {
-        $url = $this->api_base_url . $resource . '?apikey=' . $this->api_key;
+        $url = $this->api_base_url . $resource . $this->api_key;
         $client = new Client();
         $response = $client->get($url, [
             'headers' => ['Cookie' => $cookie]
